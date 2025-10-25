f96e8597bb0e9ab39db37803308eef81253d51b5
Gutemberg Ferreira <39074373+gutembergferreira@users.noreply.github.com>
2025-10-25 02:16:16 -0400

    reset

--- app/views/pieces/_piece.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! piece, :id, :description, :model, :code, :sketch, :created_at, :updated_at
-json.url piece_url(piece, format: :json)
--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
-  resources :pieces
-  resources :specifications
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
